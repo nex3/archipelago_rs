fddd9d28b7d418873898ad456b0fdcc6cfa7c464
Natalie Weizenbaum <nex342@gmail.com>
2026-01-20 00:48:26 -0800

    Remove lib doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-//! A Rust library that for the [Archipelago game randomizer](archipelago.gg), that implements the [Archipelago network protocol](https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/network%20protocol.md)
-//! Check out ArchipelagoClient for the meat of the logic
-
 mod client;
 mod connection;
 mod connection_options;
